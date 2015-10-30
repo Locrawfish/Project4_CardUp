4c556a2c1239827e0bb0b6bf031d5d23cda423ca
Addison Funk <funk.addison@gmail.com>
2015-10-29 21:59:01 -0400

    Delete button now takes away data

--- client/app/contact/contact.service.js
+++ client/app/contact/contact.service.js
@@ -19,11 +19,12 @@
     };
 
 
-    that.removeCard = function(card) {
-      var index = myUser.cards.indexOf(card);
-      myUser.card.splice(index, 1);
-      $http.delete('/api/cards'+ card._id, card);
-      res.redirect('/cards');
+    that.removeCard = function(card, res) {
+      var index = myUser.card;
+      myUser.cards.splice(index, 1);
+      $http.delete('/api/cards/'+ card._id, card);
+      console.log(card);
+      res.redirect('http://localhost:9000/cards');
     };
 
 
